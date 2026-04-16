6a95cf22b6b56a54e6fb8758a514ee381ae7122e
shersingh <luckyshera95@gmail.com>
2026-04-16 12:19:30 +0530

    added user model

--- models.py
+++ models.py
@@ -1,1 +1,10 @@
-from pydantic import BaseModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    email: str
+    password: str
+    last_login: str
+
