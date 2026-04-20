35104938358c1f4c8f4c058d9526eb27d45e5dff
shersingh <luckyshera95@gmail.com>
2026-04-20 10:38:06 +0530

    added endpoint for  fetching all notifications

--- endpoints/notification_endpoints.py
+++ endpoints/notification_endpoints.py
@@ -2,3 +2,7 @@
 @app.post("/notification/mark/read/{note_id}", status_code=status.HTTP_200_OK, tags=[Tags.notification])
 async def mark_notification_as_read(note_id:int, session: SessionDep, user: Annotated[User, Depends(is_authenticated)]):
     pass
+
+@app.get("/notifications/unead", tags=[Tags.notification])
+async def get_notifications_list(session: SessionDep, user: Annotated[User, Depends(is_authenticated)]):
+    pass
