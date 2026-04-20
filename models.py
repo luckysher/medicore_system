d8d7a03fc389e43002a1a798ff45a89f4de36b9c
shersingh <luckyshera95@gmail.com>
2026-04-20 12:17:03 +0530

    added base model for all models

--- models.py
+++ models.py
@@ -1,18 +1,21 @@
 from typing import Annotated, Optional
 from pydantic.json_schema import SkipJsonSchema
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Enum
 import datetime
 
-class User(SQLModel, table=True):
+
+class BaseSqlModel(SQLModel, table=True):
     id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
+    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
+
+class User(BaseSqlModel):
     name: str = Field(index=True, max_length=64, nullable=False)
     email: str = Field(unique=True, max_length=64, nullable=False)
     password: str = Field(nullable=False)
     active: Annotated[bool, SkipJsonSchema()] = Field(default=False, nullable=False)
     is_staff: Annotated[bool, SkipJsonSchema()] = Field(default=True, nullable=False)
     is_admin: Annotated[bool, SkipJsonSchema()] = Field(default=False, nullable=False)
-    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
     last_login: Annotated[datetime.datetime | None, SkipJsonSchema()] = Field(default=None)
 
 
@@ -21,12 +24,9 @@ class UserLoginCredential(BaseModel):
     email: str
     password: str
 
-class Notification(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class Notification(BaseSqlModel):
     sender_id: Optional[int] = Field(default=None, foreign_key="user.id")
     receiver_id: Optional[int] = Field(default=None, foreign_key="user.id")
     title: str = Field(max_length=64, default="")
     description: str = Field(max_length=128, default="")
     read: bool = Field(default=False)
-    created_at: datetime.datetime = Field(default=datetime.datetime.now())
-
