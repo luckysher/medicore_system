73147f4c7eb0aa61e39d396d967fa232d29ae293
shersingh <luckyshera95@gmail.com>
2026-04-22 20:25:52 +0530

    added and updated various models

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from typing import Annotated, Optional
+from typing import Annotated, Optional, Literal
 from pydantic.json_schema import SkipJsonSchema
-from pydantic import BaseModel
-from sqlmodel import Field, SQLModel, Enum
+from pydantic import BaseModel, ConfigDict
+from sqlmodel import Field, SQLModel
+from enum import Enum
 import datetime
 
 class AppointmentStatus(str, Enum):
@@ -10,11 +11,9 @@ class AppointmentStatus(str, Enum):
     CANCELLED = "cancelled"
     COMPLETED = "completed"
 
-class BaseSqlModel(SQLModel, table=True):
-    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
-    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
 
-class User(BaseSqlModel):
+class User(SQLModel, table=True):
+    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
     name: str = Field(index=True, max_length=64, nullable=False)
     email: str = Field(unique=True, max_length=64, nullable=False)
     password: str = Field(nullable=False)
@@ -22,39 +21,63 @@ class User(BaseSqlModel):
     is_staff: Annotated[bool, SkipJsonSchema()] = Field(default=True, nullable=False)
     is_admin: Annotated[bool, SkipJsonSchema()] = Field(default=False, nullable=False)
     last_login: Annotated[datetime.datetime | None, SkipJsonSchema()] = Field(default=None)
-
+    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
 
 class UserLoginCredential(BaseModel):
 
     email: str
     password: str
 
-class Notification(BaseSqlModel):
+class AppointmentSearchInput(BaseModel):
+
+    patient_name: str
+    contact_no: str
+
+class AppointmentUpdatedStatus(BaseModel):
+
+    appo_id: str
+    new_status: str
+
+class Notification(SQLModel, table=True):
+    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
     sender_id: Optional[int] = Field(default=None, foreign_key="user.id")
     receiver_id: Optional[int] = Field(default=None, foreign_key="user.id")
     title: str = Field(max_length=64, default="")
     description: str = Field(max_length=128, default="")
     read: bool = Field(default=False)
+    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
 
 #  Appointment
-class Appointment(BaseSqlModel):
+class Appointment(SQLModel, table=True):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
     patient_name: str = Field(max_length=64, nullable=False, default="")
     # contacts
-    contact_no: int = Field(nullable=False)
-    home_contact_no: int = Field(nullable=False)
+    contact_no: str = Field(nullable=False, default="")
+    home_contact_no: Optional[str]
     disease: str = Field(max_length=64, nullable=False, default="")
-    doctor: Optional[int] = Field(default=None, foreign_key="doctor.id")
-    status: AppointmentStatus = Field(default=AppointmentStatus.SCHEDULED, nullable=False)
+    doctor: int = Field(default=None, foreign_key="doctor.id")
+    status: Annotated[AppointmentStatus, SkipJsonSchema()] = Field(default=AppointmentStatus.SCHEDULED)
+    appointment_date: datetime.datetime = Field(default=datetime.datetime.now())
+    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
 
-class Specialty(BaseSqlModel):
+class Speciality(SQLModel, table=True):
+    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
     name: str = Field(max_length=64, nullable=False, default="")
+    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
 
-class SubSpecialty(BaseSqlModel):
+class SubSpeciality(SQLModel, table=True):
+    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
     name: str = Field(max_length=64, nullable=False, default="")
     speciality: Optional[int] = Field(default=None, foreign_key="speciality.id")
+    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
 
-class Doctor(BaseSqlModel):
+class Doctor(SQLModel, table=True):
+    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
     name: str = Field(max_length=64, nullable=False, default="")
     email: str = Field(unique=True, max_length=64, nullable=False, default="")
-    contact_no: int = Field(nullable=False)
-    specialization: Optional[int] = Field(default="", foreign_key="subspeciality.id")
+    contact_no: str = Field(nullable=False)
+    speciality: Optional[int] = Field(default="", foreign_key="speciality.id")
+    sub_speciality: Optional[int] = Field(default="", foreign_key="subspeciality.id")
+    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
