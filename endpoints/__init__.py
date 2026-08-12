250109ffb09ae74116c4fc195fa9a5f1df2137f2
shersingh <luckyshera95@gmail.com>
2026-08-12 09:23:12 +0530

    added import for endpoints

---
+++ endpoints/__init__.py
@@ -0,0 +1,10 @@
+from endpoints.auth_endpoints import *
+from endpoints.users_endpoints import *
+from endpoints.notification_endpoints import *
+from endpoints.appointment_endpoints import *
+from endpoints.doctor_endpoints import *
+from endpoints.speciality_endpoints import *
+from endpoints.disease_endpoints import *
+from endpoints.inpatient_unit_endpoints import *
+
+
